52b7035ead5596a81c0c3bb3f6d454a2e5e2b897
Kaung Htet <51477974+just-kaunghtet@users.noreply.github.com>
2024-01-01 10:48:42 +0630

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -4,8 +4,7 @@ import {
   RouterProvider,
   createBrowserRouter,
   createRoutesFromElements,
-  Route,
-  Link
+  Route
 } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"

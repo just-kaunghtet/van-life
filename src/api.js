68fd97caf69e36bc1951a711099b685c2684b1fd
Kaung Htet <51477974+just-kaunghtet@users.noreply.github.com>
2024-01-01 12:42:41 +0630

    Update api.js

--- src/api.js
+++ src/api.js
@@ -51,19 +51,3 @@ export async function getHostVans() {
     return dataArr
 }
 
-export async function loginUser(creds) {
-    const res = await fetch("/api/login",
-        { method: "post", body: JSON.stringify(creds) }
-    )
-    const data = await res.json()
-
-    if (!res.ok) {
-        throw {
-            message: data.message,
-            statusText: res.statusText,
-            status: res.status
-        }
-    }
-
-    return data
-}

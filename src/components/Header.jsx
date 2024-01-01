6b935c932e56e59b7633f4907a2a21ac2a603e98
Kaung Htet <51477974+just-kaunghtet@users.noreply.github.com>
2024-01-01 12:39:24 +0630

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,11 +7,6 @@ export default function Header() {
         textDecoration: "underline",
         color: "#161616"
     }
-    
-    function fakeLogOut() {
-        localStorage.removeItem("loggedin")
-    }
-    
     return (
         <header>
             <Link className="site-logo" to="/">#VanLife</Link>
@@ -41,7 +36,6 @@ export default function Header() {
                         alt=""
                     />
                 </Link>
-                <button onClick={fakeLogOut}>X</button>
             </nav>
         </header>
     )
